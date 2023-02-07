fee54d13230a6f83e5a909f072ed10a810e115ac
xTheusBuildings <matheus.silva@buildings.com.br>
2023-02-07 14:18:33 -0300

    Typescript

---
+++ Typescript/Tipagem.ts
@@ -0,0 +1,19 @@
+
+// Declaração explícita
+let texto: string;
+
+// Podemos declarar arrays de números, assim como de outros tipos
+let arrayDeNumeros: number[]; 
+arrayDeNumeros = [1,4,5];
+
+// Array de strings
+let arrayDeStrings: string[];
+arrayDeStrings = ['Arroz', 'Feijão'];
+
+// Também podemos declarar tipos de arrays com esta sintaxe abaixo
+let pontos: Array<number>; 
+pontos = [5,4,7];
+
+// Segunda forma de declaração array de strings
+let users: Array<string>; 
+users = ['5','4','7'];
